6e5280128fae7a0b6892dcfedea2ae695ed2386f
Adhvaith <adhvaith.thurvas@gmail.com>
2024-02-06 20:15:44 -0800

    Added vitals class file

---
+++ vitals.py
@@ -0,0 +1,6 @@
+# Basic skeleton template for vitals class
+
+class vitals:
+    def __init__(self, internal_temp: int) -> None:
+        self.internal_temp = internal_temp # Just an example variable for how variables representing vitals should be declared
+
